f56e7c1de90e8f987ccf8d599a47e09cf2302909
Ozgur Ozcitak <ozcitak@yahoo.com>
2018-12-14 12:09:37 +0300

    Rename powershell script.

--- Documentation/serveDocs.ps1
+++ Documentation/serveDocs.ps1
@@ -1,3 +1,6 @@
+$env:VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
+$env:VisualStudioVersion="15.8"
+
 $docfx = "..\packages\docfx.console.2.40.4\tools\docfx.exe"
 
 Start-Process $docfx -ArgumentList "docfx.json --serve"
