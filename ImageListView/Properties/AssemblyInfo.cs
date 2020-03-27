3641e741fd27851eed927363e34db078f2da7f20
Ozgur Ozcitak <ozcitak@yahoo.com>
2020-03-27 16:11:35 +0300

    Bump version

--- ImageListView/Properties/AssemblyInfo.cs
+++ ImageListView/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.8.0.0")]
-[assembly: AssemblyFileVersion("13.8.0.0")]
+[assembly: AssemblyVersion("13.8.1.0")]
+[assembly: AssemblyFileVersion("13.8.1.0")]
